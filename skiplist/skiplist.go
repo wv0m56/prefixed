09bd764697fbc2224ead9a2d64e02a2a30354df1
Kiki Sugiaman <ksugiaman@gmail.com>
2018-07-18 16:02:53 +1000

    skiplist insert+replace logic

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -27,17 +27,23 @@ func (s *Skiplist) Init() {
 // It overwrites existing key if it already exists.
 // Upsert does nothing if key == "".
 func (s *Skiplist) Upsert(key string, val []byte) {
+
 	if key == "" {
 		return
 	}
 	e := newElem(key, val)
 	s.mu.Lock()
 	defer s.mu.Unlock()
+
 	if s.size == 0 {
+
 		for i := 0; i < len(e.nexts); i++ {
+
 			s.front[i] = e
 		}
+
 	} else {
+
 		s.searchAndUpsert(e)
 	}
 	s.size++
@@ -55,16 +61,22 @@ func (s *Skiplist) search(e *Element) (left []*Element, iter *Element) {
 	for h := maxHeight - 1; h >= 0; h-- { // level descending loop
 
 		if h == maxHeight-1 || left[h+1] == nil {
+
 			iter = s.front[h]
+
 		} else {
+
 			left[h] = left[h+1]
 			iter = left[h+1].nexts[h]
 		}
 
 		for { // forward loop
+
 			if iter == nil || e.key <= iter.key {
 				break // break the forward loop and go down a level
+
 			} else {
+
 				left[h] = iter
 				iter = iter.nexts[h]
 			}
@@ -75,9 +87,13 @@ func (s *Skiplist) search(e *Element) (left []*Element, iter *Element) {
 
 // right is a redundant but cheap-to-have piece of information.
 func (s *Skiplist) insert(left []*Element, e, right *Element) {
+
 	if right != nil && e.key == right.key {
+
 		s.replace(left, e, right)
+
 	} else {
+
 		s.insertBetween(left, e, right)
 	}
 }
@@ -87,19 +103,76 @@ func (s *Skiplist) insertBetween(left []*Element, e, right *Element) {
 	for i := 0; i < len(e.nexts); i++ {
 
 		if right != nil && i < len(right.nexts) {
+
 			e.nexts[i] = right
+
 		} else {
+
 			if left[i] != nil {
+
 				e.nexts[i] = left[i].nexts[i]
+
 			} else {
+
 				e.nexts[i] = s.front[i]
 			}
 		}
 
-		if left[i] == nil {
-			s.front[i] = e
-		} else {
-			left[i] = e
+		s.reassignLeftAtIndex(i, left, e)
+	}
+}
+
+func (s *Skiplist) replace(left []*Element, e, right *Element) {
+
+	if right == nil {
+
+		for i := 0; i < len(e.nexts); i++ {
+
+			e.nexts[i] = right
+			s.reassignLeftAtIndex(i, left, e)
+		}
+	} else {
+
+		for i := 0; i < max(len(e.nexts), len(right.nexts)); i++ {
+
+			if i < len(e.nexts) { // up to equal height, damn zero based index
+
+				if i < len(right.nexts) {
+
+					e.nexts[i] = right.nexts[i]
+
+				} else {
+
+					e.nexts[i] = left[i]
+				}
+				s.reassignLeftAtIndex(i, left, e)
+
+			} else {
+
+				s.reassignLeftAtIndex(i, left, right.nexts[i])
+			}
 		}
 	}
-} // TODO: check on paper
+}
+
+func (s *Skiplist) reassignLeftAtIndex(i int, left []*Element, e *Element) {
+	if left[i] == nil {
+		s.front[i] = e
+	} else {
+		left[i] = e
+	}
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
